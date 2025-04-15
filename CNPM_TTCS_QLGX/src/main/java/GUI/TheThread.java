42b78bcc1204a22d2053075028efca2e304379cf
TranManhDuy <trandomanhduy2874@gmail.com>
2025-04-15 18:55:20 +0700

    Create Thread

---
+++ CNPM_TTCS_QLGX/src/main/java/GUI/TheThread.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author manhh
+ */
+public class TheThread extends Thread {
+    @Override
+    public void run() {}
+}
